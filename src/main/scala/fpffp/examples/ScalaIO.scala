0f61652a634d9b4a32c23c11970b322353ac8b3a
Walter Chang <weihsiu@gmail.com>
2022-10-04 01:48:10 +0800

    some more refinements

--- src/main/scala/fpffp/examples/ScalaIO.scala
+++ src/main/scala/fpffp/examples/ScalaIO.scala
@@ -11,10 +11,12 @@ def putLine(line: String): IO[Unit] = IO.delay(println(line))
 
 @main
 def ScalaIO() =
-  val name =
+  val name: IO[String] =
     for
       _ <- putLine("Please enter your name:")
       name <- getLine()
       _ <- putLine(s"hey, $name, you have such beautiful name!")
     yield name
   println(name.unsafePerformIO())
+
+  val ios = List(putLine("hello"), putLine("world"), putLine("how are you"))
